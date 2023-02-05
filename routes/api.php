0a9356bf8f01faa661dd01fa495f879ad39b67fd
ukan <khnz.97@gmail.com>
2023-02-05 19:38:42 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//test
 
 Route::apiResource('produkts',ProduktController::class);
 Route::apiResource('studdents',StuddentsController::class);
