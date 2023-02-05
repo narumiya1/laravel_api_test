db8cbadbaf6caaffd3c23244f4445c0bf52b94fb
ukan <khnz.97@gmail.com>
2023-02-05 19:40:31 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 //test
+//test2
+
 
 Route::apiResource('produkts',ProduktController::class);
 Route::apiResource('studdents',StuddentsController::class);
